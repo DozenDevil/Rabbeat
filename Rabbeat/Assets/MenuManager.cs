bd2607e01ecc613af135a3c2817fd10fd88ed1af
DozenDevil <ivashka-seregin@mail.ru>
2023-11-24 23:15:19 +0300

    Добавил меню

---
+++ Rabbeat/Assets/MenuManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitted successfully!");
+        Application.Quit();
+    }
+}
